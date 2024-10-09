52bc88e76a473ca9a225ca490e44548a8066369a
yasaman-k <work.yasaman.me@gmail.com>
2024-10-09 17:39:19 +0330

    refactor: Rename JS files to TSX and remove unused default files

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AppRoot from './app';
+
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement as HTMLDivElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <AppRoot />
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+}
+else {
+  console.error('Root element not found');
+}
+
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
