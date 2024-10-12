b23213acfd0cc35e38afe82ee039581a788ae71e
yasaman-k <work.yasaman.me@gmail.com>
2024-10-12 09:49:40 +0330

    fix: file name

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AppRoot from './app';
 
 const rootElement = document.getElementById('root')
