1f83d29888056711d5c18a44187dcacab0c92799
yasaman-k <work.yasaman.me@gmail.com>
2024-10-13 12:11:45 +0330

    test: add routing tests for AppRoot component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import App from './app';
+import AppRoot from './app';
+import Home from './pages/home';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('Home', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/index/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Page404 component for non-existing routes', () => {
+  render(
+    <MemoryRouter initialEntries={['/non-existing-route']}>
+      <AppRoot />
+    </MemoryRouter>,
+  );
+
+  expect(screen.getByText(/page/i)).toBeInTheDocument(); // Adjust this based on actual text in Page404 component
 });
