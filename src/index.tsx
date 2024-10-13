b6006dcd26462750a1ebd5bbeec2ef91806bce8e
yasaman-k <work.yasaman.me@gmail.com>
2024-10-13 11:42:17 +0330

    chore: install react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoot from './app';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
@@ -11,7 +13,9 @@ if (rootElement) {
   root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <AppRoot />
+        <Provider store={store}>
+          <AppRoot />
+        </Provider>
       </BrowserRouter>
     </React.StrictMode>,
   );
@@ -21,9 +25,4 @@ else {
 }
 
 
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
