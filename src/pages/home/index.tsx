65ebbe164a04ced6687420d3ace3d8792ce61266
yasaman-k <work.yasaman.me@gmail.com>
2024-10-17 03:43:38 +0330

    refactor

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,12 @@
+import Counter from "../../features/counter/counter"
+import Task from "../../features/task"
+
 const Home = () => {
     return (
-        <div>index</div>
+        <>
+            <Counter />
+            <Task />
+        </>
         // sort completed - updated -
         // <button>Add Task</button>
         // <TasksList/>
