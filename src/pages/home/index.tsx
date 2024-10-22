4e18e031839c3373c5601f46cafbd885a0de60a3
yasaman-k <work.yasaman.me@gmail.com>
2024-10-22 21:47:46 +0330

    chore: remove code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,9 @@
 import Counter from "../../features/counter/counter"
-import Task from "../../features/task"
 
 const Home = () => {
     return (
         <>
             <Counter />
-            <Task />
         </>
         // sort completed - updated -
         // <button>Add Task</button>
